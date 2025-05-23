10e0220ccccf89078c5a250a9c84fe22618fcb27
satyajit-patel <satyajitpatel770001@gmail.com>
2025-05-23 17:18:37 +0530

    render templates done

--- djangoTheDIsSilent/djangoTheDIsSilent/views.py
+++ djangoTheDIsSilent/djangoTheDIsSilent/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("wellcome to home page")
+    # return HttpResponse("wellcome to home page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("wellcome to about page")
