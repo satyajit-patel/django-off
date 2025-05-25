b9abce4edb1f3cc3930a52758bec0cbfe4f0a490
satyajit-patel <satyajitpatel770001@gmail.com>
2025-05-25 20:15:06 +0530

    insert done

--- djangoTheDIsSilent/djangoUnchained/models.py
+++ djangoTheDIsSilent/djangoUnchained/models.py
@@ -7,3 +7,6 @@ class superHeroes(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
     date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
